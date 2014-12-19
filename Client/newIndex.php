961bc82aff9e8ec8a6becaec4ca5a08b9b38aa90
pionnertch <elaion256@outlook.com>
2014-12-19 12:30:58 -0200

    new files

---
+++ Client/newIndex.php
@@ -0,0 +1,67 @@
+
+<!DOCTYPE html>
+<html lang="es" class="no-js">
+    <head>
+        <meta charset="utf-8">
+        <title>Smile Points</title>
+       
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <!-- CSS -->
+
+        <link rel='stylesheet' href='http://fonts.googleapis.com/css?family=PT+Sans:400,700'>
+        <link rel="stylesheet" href="assets/css/reset.css">
+        <link rel="stylesheet" href="assets/css/supersized.css">
+        <link rel="stylesheet" href="assets/css/style.css">
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet" />
+
+        <style type="text/css">
+       .logins{
+        padding: 2em;
+        margin:.7em;
+        display: inline-block;
+        vertical-align: top;
+       }
+       .logins:nth-child(1){
+        background-color: #3C599F;
+       /* twitter #42C5F6     google plus #CE1616*/
+       }
+
+       .logins:nth-child(2){
+        background-color: #42C5F6;
+       }
+        .logins:nth-child(3){
+
+        background-color: #CE1616;
+       }
+      .wrapper_logins{
+        display: inline-block;
+        vertical-align: top;
+      }
+
+        </style>
+
+        <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+        <!--[if lt IE 9]>
+            <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <div class="page-container">
+        <h1 style="margin-bottom: 1.5em;">Me gusta Wi-Fi</h1>
+            <h2></h2>
+<div class="wrapper-logins">
+   <div class="logins"><i class="fa fa-facebook fa-3x"></i></div>
+   <div class="logins"><i class="fa fa-twitter fa-3x"></i></div>
+   <div class="logins"><i class="fa fa-google-plus fa-3x"></i></div>
+</div>
+             
+        </div>
+        <!-- Javascript -->
+        <script src="assets/js/jquery-1.8.2.min.js"></script>
+        <script src="assets/js/supersized.3.2.7.min.js"></script>
+        <script src="assets/js/supersized-init.js"></script>
+        <script src="assets/js/scripts.js"></script>
+    </body>
+</html>
